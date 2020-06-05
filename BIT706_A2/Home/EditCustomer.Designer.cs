116f7c02880d6fd17b851b83462fb0edebe912ec
CeliaEva98 <celia.eva12345@gmail.com>
2020-06-05 13:25:12 +1200

    Edit Customers Window done

---
+++ BIT706_A2/Home/EditCustomer.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Home
+{
+    partial class EditCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonAddNew = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.labelEdit = new System.Windows.Forms.Label();
+            this.textBoxContact = new System.Windows.Forms.TextBox();
+            this.labelContact = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelIDNum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(53, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LLRT Bank";
+            // 
+            // buttonAddNew
+            // 
+            this.buttonAddNew.Font = new System.Drawing.Font("Perpetua Titling MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAddNew.Location = new System.Drawing.Point(406, 518);
+            this.buttonAddNew.Name = "buttonAddNew";
+            this.buttonAddNew.Size = new System.Drawing.Size(273, 57);
+            this.buttonAddNew.TabIndex = 20;
+            this.buttonAddNew.Text = "Add Customer";
+            this.buttonAddNew.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.LightGray;
+            this.buttonCancel.Font = new System.Drawing.Font("Perpetua Titling MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.Location = new System.Drawing.Point(45, 617);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(149, 63);
+            this.buttonCancel.TabIndex = 19;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            // 
+            // labelEdit
+            // 
+            this.labelEdit.AutoSize = true;
+            this.labelEdit.Font = new System.Drawing.Font("Perpetua Titling MT", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEdit.Location = new System.Drawing.Point(257, 141);
+            this.labelEdit.Name = "labelEdit";
+            this.labelEdit.Size = new System.Drawing.Size(352, 29);
+            this.labelEdit.TabIndex = 18;
+            this.labelEdit.Text = "Edit Existing Customer";
+            // 
+            // textBoxContact
+            // 
+            this.textBoxContact.Location = new System.Drawing.Point(319, 458);
+            this.textBoxContact.Name = "textBoxContact";
+            this.textBoxContact.Size = new System.Drawing.Size(436, 22);
+            this.textBoxContact.TabIndex = 17;
+            // 
+            // labelContact
+            // 
+            this.labelContact.AutoSize = true;
+            this.labelContact.Font = new System.Drawing.Font("Perpetua Titling MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContact.Location = new System.Drawing.Point(93, 459);
+            this.labelContact.Name = "labelContact";
+            this.labelContact.Size = new System.Drawing.Size(192, 21);
+            this.labelContact.TabIndex = 16;
+            this.labelContact.Text = "Contact Number:";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(319, 395);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(436, 22);
+            this.textBoxLastName.TabIndex = 15;
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Font = new System.Drawing.Font("Perpetua Titling MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLastName.Location = new System.Drawing.Point(93, 395);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(119, 21);
+            this.labelLastName.TabIndex = 14;
+            this.labelLastName.Text = "Last Name:";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(319, 328);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(436, 22);
+            this.textBoxFirstName.TabIndex = 13;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Font = new System.Drawing.Font("Perpetua Titling MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFirstName.Location = new System.Drawing.Point(93, 327);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(125, 21);
+            this.labelFirstName.TabIndex = 12;
+            this.labelFirstName.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 269);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(230, 21);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Customer ID Number:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelIDNum
+            // 
+            this.labelIDNum.AutoSize = true;
+            this.labelIDNum.Location = new System.Drawing.Point(319, 269);
+            this.labelIDNum.Name = "labelIDNum";
+            this.labelIDNum.Size = new System.Drawing.Size(46, 17);
+            this.labelIDNum.TabIndex = 22;
+            this.labelIDNum.Text = "label3";
+            // 
+            // EditCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(800, 711);
+            this.Controls.Add(this.labelIDNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonAddNew);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.labelEdit);
+            this.Controls.Add(this.textBoxContact);
+            this.Controls.Add(this.labelContact);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.label1);
+            this.Name = "EditCustomer";
+            this.Text = "EditCustomer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonAddNew;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label labelEdit;
+        private System.Windows.Forms.TextBox textBoxContact;
+        private System.Windows.Forms.Label labelContact;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelIDNum;
+    }
+}
