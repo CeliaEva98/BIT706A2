0850680b402748aac6db89c40fab3ee05945f3db
CeliaEva98 <celia.eva12345@gmail.com>
2020-06-05 13:31:15 +1200

    Controller and customer classes added

---
+++ BIT706_A2/Home/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    class Controller
+    {
+        public List<Customer> CustomersList = new List<Customer>();
+
+
+        public void CreateCustomer(String firstName, String lastName, int contactNumber)
+        {
+
+        }
+
+        public void DeleteCustomer(int customerNumber)
+        {
+
+        }
+
+        public void EditCustomer(int customerNumber)
+        {
+
+        }
+
+        public void FindCustomer(int customerNumber)
+        {
+
+        }
+    }
+}
---
+++ BIT706_A2/Home/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home
+{
+    class Customer
+    {
+        int customerID;
+        String customerFirstName;
+        String customerLastName;
+        int customerContactNum;
+    }
+}
