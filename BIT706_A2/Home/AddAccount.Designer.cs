58767b32ac963da95d2941c9ae3a12dc734a1fc0
CeliaEva98 <celia.eva12345@gmail.com>
2020-06-25 23:07:44 +1200

    Added AddAccount form

---
+++ BIT706_A2/Home/AddAccount.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Home
+{
+    partial class AddAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AddAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 672);
+            this.Name = "AddAccount";
+            this.Text = "AddAccount";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
